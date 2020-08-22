0d25040211d0bac3e1d5bc76272ab7e495c1c00a
leandrolavore <leandro@desygner.com>
2020-08-22 19:29:55 +1000

    atarted new carousel

--- src/pages/components/card.js
+++ src/pages/components/card.js
@@ -11,17 +11,19 @@ export default class Card extends Component {
             <li className={`uk-card uk-card-default uk-card-body ${this.props.className
             }`}>
                 <h3 className="uk-card-title">{this.props.name}</h3>
-                <video src={`${process.env.S3_BUCKET}/${this.props.name}.mp4`} 
-                        className="video"
-                        autoPlay 
-                        loop 
-                        controls 
-                        muted
-                >   
-                </video>
-                <div className="cardLinks-container">
-                    <a>Live</a>
-                    <a>Git Hub</a>
+                <div className="uk-flex">
+                    <video src={`${process.env.S3_BUCKET}/${this.props.name}.mp4`} 
+                            className="video"
+                            autoPlay 
+                            loop 
+                            controls 
+                            muted      
+                    >   
+                    </video>
+                    <div className="cardLinks-container">
+                        <a>Live</a>
+                        <a>Git Hub</a>
+                    </div>
                 </div>
             </li>
         )
--- src/pages/components/react-projects.js
+++ src/pages/components/react-projects.js
@@ -15,66 +15,24 @@ export default class ReactProjects extends Component {
             ],
             focusedCard: 0
         }
-        this.decreaseFocusedCard = this.decreaseFocusedCard.bind(this);
-        this.incrementFocusedCard = this.incrementFocusedCard.bind(this);
     }
-
-    decreaseFocusedCard() {
-        this.setState({focusedCard: --this.state.focusedCard});
-    }
-
-    incrementFocusedCard() {
-        this.setState({focusedCard: ++this.state.focusedCard});
-    }
-
 
     render() {
-        let card = <Card name={this.state.videos[this.state.focusedCard]}
-                         className="card"/>;
 
-        let prevCard = this.state.focusedCard > 0   ? <Card 
-                                                        name={this.state.videos[this.state.focusedCard - 1]}
-                                                        className="card prevCard animateCard"/>
-                                                    : '';
-
-        let nextCard = this.state.focusedCard < 3   ? <Card 
-                                                        name={this.state.videos[this.state.focusedCard + 1]}
-                                                        className="card nextCard animateCard"/> 
-                                                    : '';;
-
-        let upperButton = this.state.focusedCard > 0
-            ? <button onClick={this.decreaseFocusedCard} className="uk-button-default button uk-align-center">
-                <i uk-icon="chevron-up"></i>
-            </button>
-            : '';
-
-        let bottomButton = this.state.focusedCard < this.state.videos.length - 1
-            ? <button onClick={this.incrementFocusedCard} className="uk-button-default scroll-down button uk-align-center">
-                <i uk-icon="chevron-down"></i>
-              </button>
-
-            : '';
+        let cards = this.state.videos.map((video, index)=>{ 
+            return <Card    name={video}
+                            className="uk-card uk-card-body uk-card-default uk-card-large"
+                            uk-slider-item={index}/>;  
+        });
 
         return (
-            <div className="uk-flex uk-height-1-1 port-container">
-                <div className="uk-container uk-container-large uk-width-1-2 text">
-                    This was the first App that I started from scratch, not following any tutorial, so it is when I got a more conscious understanding of Life-Cycle methods, and functional programming. This one forced me to go to the documentation, and to abandon the preconceptions I had coming from Object Oriented programming like Java.
-
-                    I wanted to create a News App, that could identify the user's location and serve news from that specific country.
-
-                    So I used the nav-geolocation coordinates from the browser, then I used the reverse-geocoding service from Google Maps API, to obtain the country's name. Then I would contact the News API, get the json data result, and FILTER the news getting only the ones that contained that country's name in the response. (In the Json file "country": "Argentina"). In order to make it work properly and avoid any loops or my renders falling behind, I put all that process in the ComponentDidMount method. I didn't store any value as property, just kept it functional.
-
+                <div className="uk-slider">
+                    <a href="" className="uk-slidenav-large uk-position-center-left-out" uk-slidenav-previous></a>
+                    <a href="" className="uk-slidenav-large uk-position-center-right-out" uk-slidenav-next></a>
+                    <ul className="uk-slider-items">
+                        {cards}
+                    </ul>
                 </div>
-                <hr className="uk-divider-vertical uk-height-1-1 vertical"></hr>
-                <div className="overlay"></div>
-                <div className="uk-width-1-2 slider">
-                     {upperButton}
-                        {prevCard}
-                            {card}
-                        {nextCard}
-                    {bottomButton}
-                </div>
-            </div>
         )
     }
 }
