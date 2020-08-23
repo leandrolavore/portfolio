12a9dbbdce21d5a903aed03cddc56b0743fd58c2
leandrolavore <leandro@desygner.com>
2020-08-23 15:22:19 +1000

    added s3

--- src/pages/components/python-card.js
+++ src/pages/components/python-card.js
@@ -4,22 +4,41 @@ export default class PythonCard extends Component{
 
     constructor(props){
         super(props);
+
+        this.state = {
+            isLoading: true, 
+            project: {}
+         }
+    }
+
+    componentDidMount() {
+        fetch(`${process.env.S3_BUCKET}/jsons/${this.props.title.toLowerCase()}.json`)
+            .then(data => data.json())
+            .then((json) => {
+                this.setState({project: json})
+            })
     }
 
     render(){
-        return(
-            <div className='uk-width-1-4 uk-flex uk-flex-column python-band'>
-                <i className="uk-flex-top python-icon" uk-icon="search"></i>
-                <div>
-                    <h1 className="uk-text-lead uk-text-center python-title">
-                    <span className="python-span">{this.props.title}</span>
-                    </h1>
-                    <p className="uk-text-meta python-text">
-                        {this.props.text}
-                    </p>
-                </div>
-                <a className="uk-flex-bottom uk-button uk-button-default uk-button-small" href="">See the code</a>
-            </div>  
+        const {project, isLoading} = this.state;
+
+        return( project != {}
+                    ?
+                        <div className='uk-width-1-4 uk-flex uk-flex-column python-band'>
+                            <i className="uk-flex-top python-icon" uk-icon="search"></i>
+                            <div>
+                                <h1 className="uk-text-lead uk-text-center python-title">
+                                <span className="python-span">{project.name}</span>
+                                </h1>
+                                <p className="uk-text-meta python-text">
+                                    {project.desc}
+                                </p>
+                            </div>
+                            <a className="uk-flex-bottom uk-button uk-button-default uk-button-small" href={project.git}>See the code</a>
+                        </div>  
+                    :
+
+                        <h1>Loading ...</h1>
         );
     }
 }
--- src/pages/components/python-projects.js
+++ src/pages/components/python-projects.js
@@ -6,21 +6,27 @@ export default class PythonProjects extends Component {
     constructor(){
         super();
         this.state = {   
-                title : "Web-scrapper",
-                text : `This is the web-scrapper I used to create my Tower services DB. 
-                The program reads through an XML site with namespaces. 
-                The data is directly parsed into a Json file that I uploaded to Mongo`,
-                src : 'src'
+            python_projects : {
+                titles: [
+                    "Web-scrapper",
+                    "Automated-mail",
+                    "Voice-assistant",
+                    "Web-scrapper-2"
+                ],
+            }
         };
     }
 
     render(){
+        const {python_projects} = this.state;
+
+        let cards = python_projects.titles.map((title, index) =>{
+            return <PythonCard title={title} key={index}/> 
+        })
+
         return(
             <div className="uk-height-1-1 uk-flex flex-container-python">
-                <PythonCard title={this.state.title} text={this.state.text} src={this.state.src}/> 
-                <PythonCard title={this.state.title} text={this.state.text} src={this.state.src}/>
-                <PythonCard title={this.state.title} text={this.state.text} src={this.state.src}/>
-                <PythonCard title={this.state.title} text={this.state.text} src={this.state.src}/>
+                {cards}
             </div>
 
         );  
