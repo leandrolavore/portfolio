b3733203164f4f94c2cc2aec1238df94091b41f7
leandrolavore <leandro@desygner.com>
2020-10-04 17:55:09 +1000

    under construction for experience

--- src/pages/components/experience.js
+++ src/pages/components/experience.js
@@ -9,85 +9,88 @@ export default class Experience extends Component {
     render(){
         
         return(
-            <div className="uk-grid uk-grid-small uk-width-1-1 uk-margin-large exp-grid">
-                <div className="uk-width-2-3">
-                    <div className="uk-card uk-card-default uk-card-body  uk-height-1-1">
-                        <h3 className="uk-heading-bullet">First steps</h3>
-                        <p>
-                            This is the web-scrapper I used to create my Tower services DB.
-                            The program reads through an XML site with namespaces.
-                            The data is directly parsed into a Json file that I uploaded to Mongo
-                        </p>
+            <div>
+                <div className="under-construction"></div>
+                <div className="uk-grid uk-grid-small uk-width-1-1 uk-margin-large exp-grid">
+                    <div className="uk-width-2-3">
+                        <div className="uk-card uk-card-default uk-card-body  uk-height-1-1">
+                            <h3 className="uk-heading-bullet">First steps</h3>
+                            <p>
+                                This is the web-scrapper I used to create my Tower services DB.
+                                The program reads through an XML site with namespaces.
+                                The data is directly parsed into a Json file that I uploaded to Mongo
+                            </p>
+                        </div>
                     </div>
-                </div>
-                <div className="uk-width-1-3">
-                    <div className="uk-card uk-card-default uk-card-body">
-                        <h3 className="uk-heading-bullet">Python</h3>
-                        <p>
-                            This is the web-scrapper I used to create my Tower services DB.
-                            The program reads through an XML site with namespaces.
-                            The data is directly parsed into a Json file that I uploaded to Mongo
-                        </p>
+                    <div className="uk-width-1-3">
+                        <div className="uk-card uk-card-default uk-card-body">
+                            <h3 className="uk-heading-bullet">Python</h3>
+                            <p>
+                                This is the web-scrapper I used to create my Tower services DB.
+                                The program reads through an XML site with namespaces.
+                                The data is directly parsed into a Json file that I uploaded to Mongo
+                            </p>
+                        </div>
                     </div>
-                </div>
-                <div className="uk-width-1-1 uk-margin-medium-top">
-                    <div className="uk-card uk-card-default uk-card-body">
-                        <h3 className="uk-heading-bullet">First job:</h3>
-                        <p>
-                            This is the web-scrapper I used to create my Tower services DB.
-                            The program reads through an XML site with namespaces.
-                            The data is directly parsed into a Json file that I uploaded to Mongo
-                        </p>
+                    <div className="uk-width-1-1 uk-margin-medium-top">
+                        <div className="uk-card uk-card-default uk-card-body">
+                            <h3 className="uk-heading-bullet">First job:</h3>
+                            <p>
+                                This is the web-scrapper I used to create my Tower services DB.
+                                The program reads through an XML site with namespaces.
+                                The data is directly parsed into a Json file that I uploaded to Mongo
+                            </p>
+                        </div>
                     </div>
-                </div>
-                <div className="uk-width-1-3  uk-margin-small-top">
-                    <div className="uk-card uk-card-default uk-card-body">
-                        <h3 className="uk-heading-bullet">Debugging</h3>
-                        <p>
-                            This is the web-scrapper I used to create my Tower services DB.
-                            The program reads through an XML site with namespaces.
-                            The data is directly parsed into a Json file that I uploaded to Mongo
-                        </p>
+                    <div className="uk-width-1-3  uk-margin-small-top">
+                        <div className="uk-card uk-card-default uk-card-body">
+                            <h3 className="uk-heading-bullet">Debugging</h3>
+                            <p>
+                                This is the web-scrapper I used to create my Tower services DB.
+                                The program reads through an XML site with namespaces.
+                                The data is directly parsed into a Json file that I uploaded to Mongo
+                            </p>
+                        </div>
                     </div>
-                </div>
-                <div className="uk-width-1-3  uk-margin-small-top">
-                    <div className="uk-card uk-card-default uk-card-body">
-                        <h3 className="uk-heading-bullet">Refactoring</h3>
-                        <p>
-                            This is the web-scrapper I used to create my Tower services DB.
-                            The program reads through an XML site with namespaces.
-                            The data is directly parsed into a Json file that I uploaded to Mongo
-                        </p>
+                    <div className="uk-width-1-3  uk-margin-small-top">
+                        <div className="uk-card uk-card-default uk-card-body">
+                            <h3 className="uk-heading-bullet">Refactoring</h3>
+                            <p>
+                                This is the web-scrapper I used to create my Tower services DB.
+                                The program reads through an XML site with namespaces.
+                                The data is directly parsed into a Json file that I uploaded to Mongo
+                            </p>
+                        </div>
                     </div>
-                </div>
-                <div className="uk-width-1-3 uk-margin-small-top">
-                    <div className="uk-card uk-card-default uk-card-body">
-                        <h3 className="uk-heading-bullet">Modeling</h3>
-                        <p>
-                            This is the web-scrapper I used to create my Tower services DB.
-                            The program reads through an XML site with namespaces.
-                            The data is directly parsed into a Json file that I uploaded to Mongo
-                        </p>
+                    <div className="uk-width-1-3 uk-margin-small-top">
+                        <div className="uk-card uk-card-default uk-card-body">
+                            <h3 className="uk-heading-bullet">Modeling</h3>
+                            <p>
+                                This is the web-scrapper I used to create my Tower services DB.
+                                The program reads through an XML site with namespaces.
+                                The data is directly parsed into a Json file that I uploaded to Mongo
+                            </p>
+                        </div>
                     </div>
-                </div>
-                <div className="uk-width-1-2  uk-margin-small-top">
-                    <div className="uk-card uk-card-default uk-card-body">
-                        <h3 className="uk-heading-bullet">PHP</h3>
-                        <p>
-                            This is the web-scrapper I used to create my Tower services DB.
-                            The program reads through an XML site with namespaces.
-                            The data is directly parsed into a Json file that I uploaded to Mongo
-                        </p>
+                    <div className="uk-width-1-2  uk-margin-small-top">
+                        <div className="uk-card uk-card-default uk-card-body">
+                            <h3 className="uk-heading-bullet">PHP</h3>
+                            <p>
+                                This is the web-scrapper I used to create my Tower services DB.
+                                The program reads through an XML site with namespaces.
+                                The data is directly parsed into a Json file that I uploaded to Mongo
+                            </p>
+                        </div>
                     </div>
-                </div>
-                <div className="uk-width-1-2  uk-margin-small-top">
-                    <div className="uk-card uk-card-default uk-card-body">
-                        <h3 className="uk-heading-bullet">Git/CI</h3>
-                        <p>
-                            This is the web-scrapper I used to create my Tower services DB.
-                            The program reads through an XML site with namespaces.
-                            The data is directly parsed into a Json file that I uploaded to Mongo
-                        </p>
+                    <div className="uk-width-1-2  uk-margin-small-top">
+                        <div className="uk-card uk-card-default uk-card-body">
+                            <h3 className="uk-heading-bullet">Git/CI</h3>
+                            <p>
+                                This is the web-scrapper I used to create my Tower services DB.
+                                The program reads through an XML site with namespaces.
+                                The data is directly parsed into a Json file that I uploaded to Mongo
+                            </p>
+                        </div>
                     </div>
                 </div>
             </div>
